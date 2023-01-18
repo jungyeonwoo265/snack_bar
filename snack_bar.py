915b585642feb480f930ca1606879d4e97936e9d
rabozzang <rabozzang@naver.com>
2023-01-18 09:55:06 +0900

    그래프 추가중

--- snack_bar.py
+++ snack_bar.py
@@ -434,31 +434,12 @@ class WindowClass(QMainWindow, snack_bar):
         self.cellchoice = self.data.text()
 
     def showgraph(self):
-        # self.fig = plt.Figure()
-        # self.figpie = plt.Figure()
-        # self.canvas = FigureCanvas(self.fig)
-        # self.canvas2 = FigureCanvas(self.figpie)
-        # self.verticalLayout.addWidget(self.canvas)
-        # ax = self.fig.add_subplot(111)
-        #
-        # # 우리나라의 연간 1인당 국민소득을 각각 years, gdp에 저장
-        # years = [1950, 1960, 1970, 1980, 1990, 2000, 2010]
-        # gdp = [67.0, 80.0, 257.0, 1686.0, 6505, 11865.3, 22105.3]
-        #
-        # # 선 그래프를 그린다. x축에는 years값, y축에는 gdp값을 표시한다.
-        # plt.plot(years, gdp, color='green', marker='o', linestyle='solid')
-        #
-        # # 제목을 설정한다.
-        # plt.title('GDP per capita')  # 1인당 국민소득
-        #
-        # # y축에 레이블을 붙인다.
-        # plt.ylabel('dollars')
-        # plt.savefig('gdp_per_capita.png', dpi=600)
-        pass
-
-
-
-        # pass
+        self.fig = plt.Figure()
+        self.figpie = plt.Figure()
+        self.canvas = FigureCanvas(self.fig)
+        self.canvas2 = FigureCanvas(self.figpie)
+        self.verticalLayout.addWidget(self.canvas)
+        ax = self.fig.add_subplot(111)
 
 
 if __name__ == "__main__":
