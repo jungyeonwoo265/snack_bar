63dcf795f1204ffdd2708cecd5e3e45c2ca5840b
parksihyeong <si33774463@gmail.com>
2023-01-19 10:33:05 +0900

    새 상품 등록

--- snack_bar.py
+++ snack_bar.py
@@ -1,4 +1,6 @@
 import sys
+
+from PyQt5.QtGui import QIntValidator
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 import pymysql as p
@@ -18,7 +20,8 @@ matplotlib.rc('font', family='Malgun Gothic')
 # db 연결용 정보
 hos = 'localhost'
 use = 'root'
-pw = '0000'
+pw = 'qwer1234'
+
 
 
 class Thread(QThread):
@@ -181,6 +184,14 @@ class WindowClass(QMainWindow, snack_bar):
         self.manager_sales.clicked.connect(self.showgraph)
         # 관리자매출확인페이지속 취소 버튼클릭시 관리자메인페이지로 이동
         self.salesback_button.clicked.connect(self.show_back_b)
+        self.kimbap_plus.valueChanged.connect(self.calculate_bom)
+        self.tuna_kimbap_plus_2.valueChanged.connect(self.calculate_bom)
+        self.Cheese_kimbap_plus_2.valueChanged.connect(self.calculate_bom)
+        self.bokki_plus_3.valueChanged.connect(self.calculate_bom)
+        self.rabokki_plus_3.valueChanged.connect(self.calculate_bom)
+        self.Cheese_bokki_plus.valueChanged.connect(self.calculate_bom)
+        self.pig_Stew_plus_3.valueChanged.connect(self.calculate_bom)
+        self.tuna_Stew_plus.valueChanged.connect(self.calculate_bom)
 
         # 관리자메인페이지속 재고관리 버튼클릭시 재고관리 게시판으로 이동
         self.manager_inventory.clicked.connect(self.inventory_view)
@@ -189,6 +200,22 @@ class WindowClass(QMainWindow, snack_bar):
 
         # 관리자의 메인페이지속 로그아웃 버튼클릭시 로그인화면으로 이동
         self.logout_manager_button.clicked.connect(self.homepage)
+
+        # 새메뉴에 필요한 재료 넣는 리스트인데 메서드에 넣으면 거기 메서드갈때마다 초기화되서 과감히 init에 넣음
+        self.store_ingredient = []
+        # 신제품추가 버튼가면 신제품페이지로 이동
+        self.manager_sales_2.clicked.connect(self.adde_screen)
+        # 기존 메뉴 재료들 검색
+        self.food_search.clicked.connect(self.add_food)
+        # 신메뉴에 들어갈 재료 더블클릭시 단위랑 재료이름 옆으로 들어감
+        self.ingredient.cellDoubleClicked.connect(self.add_ingredient)
+        # 아래에 재료 추가됨
+        self.add_btn.clicked.connect(self.plus_ingredient)
+        # 뒤로가기로 관리자 페이지 처음으로 이동함
+        self.cancel_btn.clicked.connect(self.clearMode)
+        # 신메뉴 등록됨
+        self.confirm_btn.clicked.connect(self.confirm_food)
+
 
     def thread_action(self):
         t = Thread(self)
@@ -285,9 +312,11 @@ class WindowClass(QMainWindow, snack_bar):
             QMessageBox.critical(self, "에러", "아이디나 비밀번호가 틀립니다.")
         else:
             if self.login_infor[0][1] == '개인':
+                self.calculate_bom()
                 self.stackedWidget.setCurrentIndex(2)
             elif self.login_infor[0][1] == '사업자':
                 self.manager_page()
+        self.QandA_list.clear()
 
     def menu_counting(self):
         self.kimbap_plus.setStep(0)
@@ -307,6 +336,20 @@ class WindowClass(QMainWindow, snack_bar):
                     self.QandA_list.setItem(i, j, QTableWidgetItem(str(self.questionlist[i][j])))
             self.QandA_list.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
         self.conn.close()
+
+        # self.stackedWidget.setCurrentIndex(3)
+        # self.open_db()
+        # self.c.execute("SELECT * from snack.question")
+        # self.questionlist = self.c.fetchall()
+        # if self.questionlist:
+        #     self.QandA_list.setRowCount(len(self.questionlist))
+        #     self.QandA_list.setColumnCount(len(self.questionlist[0]))
+        #     self.QandA_list.setHorizontalHeaderLabels(['주문번호', '아이디', '내용', '시간', '답변'])
+        #     for i in range(len(self.questionlist)):
+        #         for j in range(len(self.questionlist[i])):
+        #             self.QandA_list.setItem(i, j, QTableWidgetItem(str(self.questionlist[i][j])))
+        #     self.QandA_list.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
+
 
     # 구매자가 문의하기 게시판에 글을 남겼을때
     def question_add(self):
@@ -357,6 +400,29 @@ class WindowClass(QMainWindow, snack_bar):
                 self.inventorylist.setItem(i, j, QTableWidgetItem(str(v)))
         for i, v in enumerate(head):
             self.inventorylist.setHorizontalHeaderItem(i, QTableWidgetItem(v))
+        self.conn.close()
+
+    # 식재료 자동 구매 기능
+    def ordering(self):
+        self.open_db()
+        self.c.execute(
+            f'select a.재료, if(max(a.수량) > min(b.수량), "구매", "보류"), min(b.단가) '
+            f'from bom a left join inventory b on a.재료 = b.재료 group by 재료;')
+        article = self.c.fetchall()
+        article_list = list()
+        # 재료 구매 list 작성 및 재료 구매 쿼리문 작성
+        for i in article:
+            if i[1] == '구매':
+                self.c.execute(f'update inventory set 수량 = 수량 + 구매량 where 재료 ="{i[0]}";')
+                article_list.append([i[0], i[2]])
+                self.conn.commit()
+        # 재무표에 구매 list 추가
+        if article_list:
+            for i in article_list:
+                self.c.execute(f'SELECT 주문번호, 잔액 FROM finance order by 주문번호 desc;')
+                fin = self.c.fetchone()
+                self.c.execute(f'insert into finance values("{fin[0]+1}","{i[0]}구매",0,{i[1]},{fin[1]-i[1]},now());')
+                self.conn.commit()
         self.conn.close()
 
     # 장바구니
@@ -420,6 +486,52 @@ class WindowClass(QMainWindow, snack_bar):
         self.income()
         # self.deduction()을 위해 추가
         self.deduction()
+
+    def calculate_bom(self):
+        self.open_db()
+        self.c.execute('select 상품명, min(b.수량 div a.수량) as 최대생산량  from bom a left join inventory b on a.재료 =b.재료 group by 상품명')
+        ingredient = self.c.fetchall()
+        self.conn.close()
+        print(ingredient)
+        self.kimbap_plus.setRange(0, ingredient[5][1])
+        self.tuna_kimbap_plus_2.setRange(0, ingredient[6][1])
+        self.Cheese_kimbap_plus_2.setRange(0, ingredient[7][1])
+        self.bokki_plus_3.setRange(0, ingredient[0][1])
+        self.rabokki_plus_3.setRange(0, ingredient[1][1])
+        self.Cheese_bokki_plus.setRange(0, ingredient[2][1])
+        self.pig_Stew_plus_3.setRange(0, ingredient[3][1])
+        self.tuna_Stew_plus.setRange(0, ingredient[4][1])
+        self.kimbap_max.setText('최대주문수량:' + str(ingredient[5][1]-self.kimbap_plus.value()))
+        self.tuna_kimbap_max.setText('최대주문수량:' + str(ingredient[6][1] - self.tuna_kimbap_plus_2.value()))
+        self.Cheese_kimbap_max.setText('최대주문수량:' + str(ingredient[7][1]-self.Cheese_kimbap_plus_2.value()))
+        self.bokki_max.setText('최대주문수량:' + str(ingredient[0][1]-self.bokki_plus_3.value()))
+        self.rabokki_max.setText('최대주문수량:' + str(ingredient[1][1]-self.rabokki_plus_3.value()))
+        self.Cheese_bokki_max.setText('최대주문수량:' + str(ingredient[2][1]-self.Cheese_bokki_plus.value()))
+        self.pig_Stew_max.setText('최대주문수량:' + str(ingredient[3][1]-self.pig_Stew_plus_3.value()))
+        self.tuna_Stew_max.setText('최대주문수량:' + str(ingredient[4][1]-self.tuna_Stew_plus.value()))
+
+    # 주문 금액 재무db에 저장하기
+    def income(self):
+        self.open_db()
+        self.c.execute(f"select sum(금액), min(시간) from request where 주문번호 = '{self.store}';")
+        income = self.c.fetchall()[0]
+        self.c.execute(f"select 잔액 from finance order by 주문번호 desc")
+        balance = self.c.fetchone()[0]
+        if income[0]:
+            self.c.execute(f"insert into finance values ({self.store},'{self.login_infor[0][0]}님 구매',{income[0]},0,{balance+int(income[0])},'{income[1]}')")
+            self.conn.commit()
+        self.conn.close()
+
+    # 주문 상품 bom 재고 차감
+    def deduction(self):
+        self.open_db()
+        for v in self.request_list:
+            self.c.execute(f"select 재료, 수량*{v[1]} as 소모량 from bom where 상품명='{v[0]}';")
+            consumption = self.c.fetchall()
+            for i in consumption:
+                self.c.execute(f"update inventory set 수량 = 수량 - {i[1]} where 재료 ='{i[0]}';")
+                self.conn.commit()
+        self.conn.close()
 
     # 관리자용 메인화면
     def manager_page(self):
@@ -590,6 +702,140 @@ class WindowClass(QMainWindow, snack_bar):
         self.verticalLayout.removeWidget(self.canvas)
         self.manager_page()
 
+    def show_combo(self):
+        check = QMessageBox.question(self, ' ', '등록 하겠습니까?', QMessageBox.Yes | QMessageBox.No, QMessageBox.Yes)
+        self.time = dt.datetime.now()
+        self.today = self.time.strftime('%Y-%m-%d %H:%M:%S')
+        productlist = ['-','김밥', '참치김밥', '치즈김밥','떡볶이','라볶이','치즈떡볶이','돼지김치찌개','참치김치찌개']
+        show = self.comboBox.currentText()
+        for i in range(len(productlist)):
+            if show == productlist[i]:
+                if check == QMessageBox.Yes:
+                    self.open_db()
+                    # 로그인된 고객의 아이디와 문의내용을 저장시켜준다
+                    self.c.execute(f"insert into snack.question (상품,아이디,내용,시간) values "
+                                   f"('{show}','{self.login_infor[0][0]}','{self.QandA_lineEdit.text()}','{self.today}')")
+                    self.conn.commit()
+                    QMessageBox.information(self, ' ', '문의가 등록되었습니다.')
+
+                # 문의한 내용을 리스트로 바로 보여주기 위한 커서
+                self.c.execute("SELECT * from snack.question")
+                self.questionlist = self.c.fetchall()
+                self.QandA_list.setRowCount(len(self.questionlist))
+                self.QandA_list.setColumnCount(len(self.questionlist[0]))
+                self.QandA_list.setHorizontalHeaderLabels(['상품','주문번호', '아이디', '내용', '시간', '답변'])
+                for i in range(len(self.questionlist)):
+                    for j in range(len(self.questionlist[i])):
+                        self.QandA_list.setItem(i, j, QTableWidgetItem(str(self.questionlist[i][j])))
+                # self.QandA_list.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
+                self.QandA_list.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+                self.QandA_lineEdit.clear()
+
+    def clearMode(self):                    # 이건 신메뉴 창에 있는 것들 초기화하려고 만든 메서드
+        self.ingredient_name.clear()
+        self.newrecipe.clear()
+        self.new_name.clear()
+        self.price.clear()
+        self.label_14.setText("재료이름")
+        self.unit_label.setText("")
+        self.food_name.clear()
+        self.ingredient_list.clear()
+        self.manager_page()
+
+    def plus_ingredient(self):
+        if self.label_14.text() == '재료이름':
+            QMessageBox.critical(self, '에러', '재료를 선택하세요')
+        elif self.ingredient_name.text() == '':
+            QMessageBox.critical(self, '에러', '수량을 입력하세요')
+        else:
+            add_ingredient_list = [self.label_14.text(), self.ingredient_name.text(), self.unit_label.text()]           # 재료이름, 수량, 단위를 리스트에 넣고
+            self.store_ingredient.append(add_ingredient_list)                                                           # init에 만들었던 리스트에 다시 담는다
+            self.newrecipe.setRowCount(len(self.store_ingredient))                                                      # 다시 안담으면 계속 쌓여지지않고 초기화됨
+            self.newrecipe.setColumnCount(len(self.store_ingredient[0]))
+            self.newrecipe.setEditTriggers(QAbstractItemView.NoEditTriggers)
+            self.newrecipe.setHorizontalHeaderLabels(['재료', '수량', '단위'])
+            for i in range(len(self.store_ingredient)):
+                for j in range(len(self.store_ingredient[0])):
+                    self.newrecipe.setItem(i, j, QTableWidgetItem(str(self.store_ingredient[i][j])))
+            self.open_db()
+            origin_cost = 0
+            for i in range(len(self.store_ingredient)):
+                self.c.execute(
+                    f'select 재료, (단가 / 구매량)*{int(self.store_ingredient[i][1])} 가격 from inventory where 재료 = "{self.store_ingredient[i][0]}"')
+                a = self.c.fetchall()
+                origin_cost += int(a[0][1])
+            self.conn.close()
+            self.cost_label.setText("원가:" + str(origin_cost) + "원")                                                   # 위에 원가 계산해서 넣어줌
+            print(self.store_ingredient, "erwrwefdds")
+
+    def confirm_food(self):
+        self.open_db()
+        self.c.execute(f'select 상품 from menu where 상품 = "{self.new_name.text()}"')                      # 메뉴가 중복되지 않게 메뉴리스트를 비교
+        product_name = self.c.fetchall()
+        self.conn.close()
+        if self.new_name.text() == '':
+            QMessageBox.critical(self, "에러", "상품명을 입력해주세요")
+        elif self.price.text() == '':
+            QMessageBox.critical(self, "에러", "가격을 입력해주세요")
+        elif product_name != ():
+            QMessageBox.critical(self, "에러", "이미 존재하는 메뉴입니다")
+        else:
+            self.open_db()
+            print("fewsd")                                                                                  # 위에 쓴 리스트를 다시 사용해 db에 신메뉴 등록
+            for i in range(len(self.store_ingredient)):
+                self.c.execute(
+                    f'insert into bom (상품명, 재료, 수량, 단위) values ("{self.new_name.text()}", "{self.store_ingredient[i][0]}", "{self.store_ingredient[i][1]}", "{self.store_ingredient[i][2]}")')
+                self.conn.commit()
+            self.c.execute(
+                f'insert into menu (상품, 단가, 단위) values ("{self.new_name.text()}", "{int(self.price.text())}", "개")')
+            self.conn.commit()
+            self.c.close()
+            QMessageBox.information(self, "확인", "메뉴가 등록되었습니다")
+            self.ingredient_name.clear()
+            self.newrecipe.clear()
+            self.new_name.clear()
+            self.price.clear()
+            self.label_14.setText("재료이름")
+            self.unit_label.setText("")
+
+    def add_ingredient(self):                           # 이건 라벨들에 이름 넣어주기
+        row = self.ingredient.currentRow()
+        food_name_e = self.ingredient.item(row, 0).text()
+        food_unit = self.ingredient.item(row, 2).text()
+        self.label_14.setText(food_name_e)
+        self.unit_label.setText(food_unit)
+        self.ingredient_name.clear()
+
+    def adde_screen(self):
+        self.onlyInt = QIntValidator()                  # 수량과 가격에 int형으로만 적을 수 있게 만드는 코드
+        self.ingredient_name.setValidator(self.onlyInt)
+        self.price.setValidator(self.onlyInt)
+        self.open_db()
+        self.c.execute('select 재료, 수량, 단위 from inventory')
+        input_ingredient = self.c.fetchall()
+        self.conn.close()
+        self.ingredient.setRowCount(len(input_ingredient))
+        self.ingredient.setColumnCount(len(input_ingredient[0]))
+        self.ingredient.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.ingredient.setHorizontalHeaderLabels(['재료', '수량', '단위'])
+        for i in range(len(input_ingredient)):
+            for j in range(len(input_ingredient[0])):
+                self.ingredient.setItem(i, j, QTableWidgetItem(str(input_ingredient[i][j])))
+        self.stackedWidget.setCurrentIndex(9)
+
+    def add_food(self):                                     # 기존 메뉴의 bom검색
+        self.open_db()
+        self.c.execute(f'select * from bom where 상품명 = "{self.food_name.text()}"')
+        need_ingredient = self.c.fetchall()
+        self.conn.close()
+        print(need_ingredient)
+        self.ingredient_list.setRowCount(len(need_ingredient))
+        self.ingredient_list.setColumnCount(len(need_ingredient[0]) - 1)
+        self.ingredient_list.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.ingredient_list.setHorizontalHeaderLabels(['재료', '수량', '단위'])
+        for i in range(len(need_ingredient)):
+            for j in range(len(need_ingredient[0]) - 1):
+                self.ingredient_list.setItem(i, j, QTableWidgetItem(str(need_ingredient[i][j + 1])))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
